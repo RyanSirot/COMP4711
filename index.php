9da50ce94ac3ed65f219806d9f697dcecd55cc75
Ryan <torisnayr@gmail.com>
2017-01-11 10:32:14 -0800

    first

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: To Olympus
+ * Date: 2017-01-11
+ * Time: 10:22 AM
+ */
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "hello i am alive"
+        ?>
+    </body>
+</html>
